af241e24aee76c4f3b10c93251a1a94b701dc3e1
DanielSacol010 <danieledusc@gmail.com>
2025-05-15 23:16:11 -0600

    feat: se agrega barra de filtrado visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/navbar.jsx";
 import VideoFondo from "./components/home/video.jsx";
 import  SimpleFooter  from "./components/footer.jsx";
+import FilterBar from "./components/FilterBar.jsx";
 
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
         <Navbar />
+        <FilterBar />
         <VideoFondo />
-        <h1>   Mundo</h1>
         <SimpleFooter />
     </>
   );
