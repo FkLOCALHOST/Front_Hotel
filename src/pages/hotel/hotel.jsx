5f66e4bcfa5716285e94c1b7749b5068c414a124
LC2020429 <lcordova-2020429@kinal.edu.gt>
2025-05-17 09:13:32 -0600

    Build(routes)

--- src/pages/hotel/hotel.jsx
+++ src/pages/hotel/hotel.jsx
@@ -1,27 +1,18 @@
 import React from "react";
-import Sidebar from "../../components/sideBar.jsx";
+import { Navbar } from "../../components/navbar.jsx";
+import VideoFondo from "../../components/home/video.jsx";
 import SimpleFooter from "../../components/footer.jsx";
-import HotelCard from "../../components/hotels/hotelCard.jsx";
-// import { useHoteles } from "../../shared/hooks/useHoteles.jsx";
+import HotelCard from "../../components/hotels/hotelCard";
 
-const FavoritosHotelesPage = () => {
-  const { hoteles, isFetching } = useHoteles();
-
-  if (isFetching) return <div>Cargando hoteles...</div>;
-
+const HotelPage = () => {
   return (
     <div>
-      <Sidebar />
-      {hotels.length === 0 ? (
-        <div>No hay hoteles</div>
-      ) : (
-      hotels.map((hotel) => (
-      <HotelCard
-      />
-      )) )}
+      <Navbar />
+      <VideoFondo />
+      <HotelCard />
       <SimpleFooter />
     </div>
   );
 };
 
-export default FavoritosHotelesPage;
+export default HotelPage;
